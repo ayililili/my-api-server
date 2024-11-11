c8897134f0a7751529b2fb3e59d6837640e18b43
Ayi <erictim1217@gmail.com>
2024-11-11 14:02:21 +0800

    update routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
 var express = require("express");
 var router = express.Router();
 
-// 简单的内存数据存储，用于模拟数据库
+// Simple in-memory data storage, simulating a database
 let dataStore = {};
 
-// 获取所有项目
+// Get all items
 router.get("/", function (req, res) {
   res.json(dataStore);
 });
 
-// 根据 ID 获取特定项目
+// Get a specific item by ID
 router.get("/:id", function (req, res) {
   const id = parseInt(req.params.id);
-  const item = dataStore.find((i) => i.id === id);
+  const item = dataStore[id]; // Access item directly by ID as key
   if (item) {
     res.json(item);
   } else {
@@ -20,39 +20,39 @@ router.get("/:id", function (req, res) {
   }
 });
 
-// 新增项目
+// Add a new item
 router.post("/", function (req, res) {
-  const newItem = {
-    id: req.body.id,
-    data: req.body.data,
-  };
-  dataStore[id] = data;
-  res.status(201).json(newItem);
+  const id = req.body.id;
+  const data = req.body.data;
+
+  // Add item to dataStore with ID as key
+  dataStore[id] = { id, data };
+  res.status(201).json({ id, data });
 });
 
-// // 更新项目
-// router.put('/:id', function (req, res) {
-//   const id = parseInt(req.params.id);
-//   const item = dataStore.find(i => i.id === id);
-//   if (item) {
-//     item.name = req.body.name;
-//     item.value = req.body.value;
-//     res.json(item);
-//   } else {
-//     res.status(404).json({ message: 'Item not found' });
-//   }
-// });
+// Update an item
+router.put("/:id", function (req, res) {
+  const id = parseInt(req.params.id);
+  const item = dataStore[id];
 
-// // 删除项目
-// router.delete('/:id', function (req, res) {
-//   const id = parseInt(req.params.id);
-//   const index = dataStore.findIndex(i => i.id === id);
-//   if (index !== -1) {
-//     const deletedItem = dataStore.splice(index, 1);
-//     res.json(deletedItem);
-//   } else {
-//     res.status(404).json({ message: 'Item not found' });
-//   }
-// });
+  if (item) {
+    item.data = req.body.data; // Update the `data` field as needed
+    res.json(item);
+  } else {
+    res.status(404).json({ message: "Item not found" });
+  }
+});
+
+// Delete an item
+router.delete("/:id", function (req, res) {
+  const id = parseInt(req.params.id);
+  if (dataStore[id]) {
+    const deletedItem = dataStore[id];
+    delete dataStore[id];
+    res.json(deletedItem);
+  } else {
+    res.status(404).json({ message: "Item not found" });
+  }
+});
 
 module.exports = router;
